d59664eefa7a25712f46884d31e35ed82778b2d3
Arka-98 <54178938+Arka-98@users.noreply.github.com>
2020-10-20 10:57:04 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -3,6 +3,10 @@ public class Calc {
   {
     return (a+b);
   }
+  public static int sub(int a, int b)
+  {
+    return (a-b);
+  }
   public static void main(String[] args)
   {
     System.out.println(add(10,20));
