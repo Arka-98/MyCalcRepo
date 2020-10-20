bb613ccda21aabfb9724556e0e40eca67c748e8d
Arka-98 <54178938+Arka-98@users.noreply.github.com>
2020-10-20 11:20:21 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -7,9 +7,14 @@ public class Calc {
   {
     return (a-b);
   }
+  public static int prod(int a, int b)
+  {
+    return (a*b);
+  }
   public static void main(String[] args)
   {
     System.out.println(add(10,20));
+    System.out.println(sub(10,20));
     System.out.println(sub(10,20));
   }
 }
